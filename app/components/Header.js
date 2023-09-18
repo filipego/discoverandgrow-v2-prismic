de5372862cee4bfab5d1714461eb63752bbb7397
Filipe Valente Gomes <filipego@gmail.com>
2023-09-18 12:23:01 -0400

    fixed links css

--- app/components/Header.js
+++ app/components/Header.js
@@ -13,7 +13,7 @@ export default async function Header() {
         <header className='header'>
             <Container classes='flex-space-between p-tb-2'>
                 <Link href="/">{settings.data.site_title}</Link>
-                <nav className='footer--nav'>
+                <nav className='header__nav'>
                     <ul>
                         {settings.data.navigation.map(({ label, link }) => (
                             <li key={label}>
